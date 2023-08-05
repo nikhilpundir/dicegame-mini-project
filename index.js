487b6b305e9dd390fa7accee3c7f697d6fc83ccd
NIKHIL <61958548+nikhilpundir@users.noreply.github.com>
2023-08-05 18:42:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var rand1=Math.random();
+rand1=Math.floor(rand1*6 + 1);
+document.querySelector(".img1").setAttribute("src",`./images/dice${rand1}.png`);
+
+
+var rand2=Math.random();
+rand2=Math.floor(rand2*6 + 1);
+document.querySelector(".img2").setAttribute("src",`./images/dice${rand2}.png`);
+
+
+if(rand1>rand2){
+    document.querySelector("h1").innerHTML="🚩Player 1 wins";
+} else if(rand1<rand2){
+    document.querySelector("h1").innerHTML="🚩Player 2 wins";
+}
+else{
+    document.querySelector("h1").innerHTML="🚩draw";
+}
